08633d82897afba973c5485e2437e812c20b223d
Abhilash Srivastava <abhi.srivastava456@gmail.com>
2019-06-20 12:44:25 +0530

    Add files via upload

---
+++ MainOcc2Nd.java
@@ -0,0 +1,12 @@
+package occurance2;
+
+import java.util.Scanner;
+
+public class MainOcc2Nd {
+    public static void main(String []arg){
+        occ2Nd o=new occ2Nd();
+        Scanner in=new Scanner(System.in);
+        String inGive=in.nextLine();
+        System.out.println(o.secOcc(inGive));
+    }
+}
